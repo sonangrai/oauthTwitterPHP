0538129311b1e06089a18dee891a3d67a43f34a9
Sonahang Rai <shonahangrae@gmail.com>
2020-06-16 16:12:37 +0545

    Update config.php
    Removed my key from config

--- config.php
+++ config.php
@@ -11,9 +11,9 @@ define('DB_NAME', '_ad_vr_db');
 define('DB_USER_TBL', 'a_user'); 
  
 // Twitter API configuration 
-define('TW_CONSUMER_KEY', 'hyA6gi2gmVaxDkpo39AkI2rf2'); 
-define('TW_CONSUMER_SECRET', 'D0ACZHrKPREoJkItFMyU5FmbdlaaymXjpxVyB6TCm64SY1Z1HX'); 
-define('TW_REDIRECT_URL', 'http://localhost/ad-vr/'); 
+define('TW_CONSUMER_KEY', 'yourkey'); 
+define('TW_CONSUMER_SECRET', 'yoursecret'); 
+define('TW_REDIRECT_URL', 'redirect link'); 
  
 // Start session 
 if(!session_id()){ 
